ad6916f4b6963b1bcf567bf8ffc84bcf1ecbc057
Julius Sonko <jsonko@my.edgetechacademy.edu>
2020-05-04 10:40:02 -0500

    Add project files.

---
+++ Arrays/FunWithStrings.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arrays
+{
+    class FunWithStrings
+    {
+        static void Main(string[] args)
+        {
+            string name = "Edge Tech Academy";
+            Console.WriteLine("My name is '" + name + "' and it is " + name.Length + " characters long");
+
+            bool hasTech = name.Contains("Tech");
+            Console.WriteLine("Does my name contain Tech? " + (hasTech ? "Why yes is does!" : "No Tech :-("));
+
+            bool isBoring = name.Contains("boring stuff");
+            Console.WriteLine("Does my name contain 'boring stuff'? " + (isBoring ? "Not So!" : "All exciting stuff"));
+
+            bool ending = name.EndsWith("h Academy");
+            Console.WriteLine("Does my name end with 'h Academy'? " + (ending ? "yup" : "nope"));
+
+            bool starting = name.StartsWith("Edge");
+            Console.WriteLine("Does my name start with 'Edge'? " + (starting ? "yup" : "nope"));
+
+            bool doTheyMatch = name.Equals("EDGE tech ACADemy");
+            Console.WriteLine("Are they equal? " + (doTheyMatch ? "yup" : "nope"));
+
+            bool ignoreCase = name.Equals("EDGE tech ACADemy", StringComparison.CurrentCultureIgnoreCase);
+            Console.WriteLine("Can I compare and ignore case? " + (ignoreCase ? "yup" : "nope"));
+            int eIndex = name.IndexOf("e");
+            while (eIndex >= 0)
+            {
+                Console.WriteLine("Found 'e' at: " + eIndex);
+                eIndex = name.IndexOf("e", eIndex + 1);
+            }
+            string newString = name.Insert(8, ">HELLO!<");
+            Console.WriteLine(newString);
+
+            int dIndex = name.LastIndexOf('d');
+            Console.WriteLine("Found last 'd' at: " + dIndex);
+
+            string[] aNames = name.Split(" ");
+            foreach (string str in aNames)
+            {
+                string padLeft = str.PadLeft(15, '.');
+                string padRight = str.PadRight(15, '_');
+                Console.WriteLine("Pad Left ->" + padLeft);
+                Console.WriteLine("Pad Right ->" + padRight);
+
+                Console.WriteLine("Unpad " + padLeft.Trim('.'));
+                Console.WriteLine("Unpad " + padRight.Trim('_'));
+            }
+
+            string nickName = name.Remove(4); // this is the same as: name.Substring(4, 0);
+            Console.WriteLine(nickName);
+
+            Console.WriteLine("Chop characters out of the middle: " + name.Substring(7, 8));
+
+            Console.WriteLine("Look Ma! No 'e's! " + name.Replace('e', '_'));
+
+            Console.WriteLine("UPPER CASE: " + name.ToUpper());
+            Console.WriteLine("lower case: " + name.ToLower());
+        }
+    }
+}
---
+++ Arrays/Loops.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arrays
+{
+    class Loops
+    {
+        static void Main(string[] args)
+        {
+            string[] days = { "Sunday", "Monday", "TuesDay", "Wednesday", "Thursday", "Friday", "Saturday" };
+            foreach (string day in days)
+            {
+                Console.WriteLine("The day is : " + day);
+            }
+
+            Console.WriteLine("---------------------------------------------------");
+            // This is the same as using above
+            string[] daysOfWeek = { "Sunday", "Monday", "TuesDay", "Wednesday", "Thursday", "Friday", "Saturday" };
+            for (int i = 0; i < daysOfWeek.Length; i++)
+            {
+                Console.WriteLine("The day is : " + daysOfWeek[i]);
+            }
+        }
+    }
+}
